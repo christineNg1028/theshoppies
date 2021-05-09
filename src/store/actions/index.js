62b4a3d3db484cc63aff7b04291bb5cc1b48774d
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-05-09 01:08:07 -0400

    Revert "Updates"
    This reverts commit 426af72f75e0e9b27c909def61d5bea1edd03249.

---
+++ src/store/actions/index.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+import * as types from "./types";
+
+export function fetchSearchResults(title, year, type, results) {
+  return {
+    type: types.FETCH_SEARCH_RESULTS,
+    payload: { title, year, type, results },
+  };
+}
+
+export function fetchSearchResultsAsync(title, year, type) {
+  return async function (dispatch, getState) {
+    await axios
+      .get(
+        `https://www.omdbapi.com/?s=${title}&y=${year}&type=${type}&apikey=8803b9a5`
+      )
+      .then(({ data }) => {
+        const results = data.Search;
+        dispatch(fetchSearchResults(title, year, type, results));
+      })
+      .catch((err) => console.log(err));
+  };
+}
+
+export function addNominee(movie) {
+  return {
+    type: types.ADD_NOMINEE,
+    movie,
+  };
+}
+
+export function removeNominee(movie) {
+  return {
+    type: types.REMOVE_NOMINEE,
+    movie,
+  };
+}
---
+++ src/store/reducers/index.js
@@ -0,0 +1,43 @@
+import {
+  FETCH_SEARCH_RESULTS,
+  ADD_NOMINEE,
+  REMOVE_NOMINEE,
+} from "../actions/types";
+import { combineReducers } from "redux";
+
+const initialState = {
+  title: "",
+  year: "",
+  type: "",
+  results: [],
+  nominations: [],
+};
+
+function theShoppiesReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_SEARCH_RESULTS:
+      const title = action.payload.title;
+      const year = action.payload.year;
+      const type = action.payload.type;
+      const results = action.payload.results;
+      return { ...state, title, year, type, results };
+    case ADD_NOMINEE:
+      return { ...state, nominations: [...state.nominations, action.movie] };
+    case REMOVE_NOMINEE:
+      const i = state.nominations.findIndex(
+        ({ imdbID }) => imdbID === action.movie.imdbID
+      );
+      return {
+        ...state,
+        nominations: state.nominations
+          .slice(0, i)
+          .concat(state.nominations.slice(i + 1)),
+      };
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  theShoppies: theShoppiesReducer,
+});
